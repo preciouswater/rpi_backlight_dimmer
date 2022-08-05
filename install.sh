3c35c5db73838c140dae9db07ef8b83e3a3b9741
Tony Kuo <ihankuo@gmail.com>
2022-08-05 03:50:45 +0100

    Add install script and fix dot in readme

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo cp ./timeout /usr/local/bin
+sudo cp ./lsinput /usr/local/bin
+sudo cp ./run-timeout.sh /usr/local/bin
+sudo chmod +s /usr/local/bin/timeout
+sudo chmod +s /usr/local/bin/run-timeout.sh
